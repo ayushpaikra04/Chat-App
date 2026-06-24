130f138e24fde87ab0dc365928c47941b9d20175
Ayush <ayush2004paikra@gmail.com>
2026-06-24 21:10:25 +0530

    moved sidebar HTML to react component

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h2>Chats</h2>
+            <input type="text" placeholder="search" />
+            <div className="contact">Soham</div>
+            <div className="contact">Parth</div>
+            <div className="contact">Vihaan</div>
+            <div className="contact">Vedanga</div>
+        </div>
+    );
+}
